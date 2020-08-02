77d84939d07e95647beb4155e580878cec2e8b72
Sebastian R <sebastianrosales.correo@gmail.com>
2020-08-02 13:27:30 -0300

    Capacimetro con Arduino

---
+++ Capacimetro_v3.ino
@@ -0,0 +1,32 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       Capacimetro_v3.ino
+    Created:	3/7/2018 13:16:50
+    Author:     DESKTOP-UI4LNIG\Sebastian
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+
+
+}
